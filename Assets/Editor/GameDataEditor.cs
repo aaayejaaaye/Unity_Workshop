cfd361874d55f7f278b71cbafa007d14a56c3bb7
aaayejaaaye <ajones369@student.cccs.edu>
2019-07-03 15:06:14 -0600

    updated readme and added GUI controls

---
+++ Assets/Editor/GameDataEditor.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;//Acess editor Window
+using System.IO;// works with files
+
+public class GameDataEditor : EditorWindow 
+{
+    public GameData gameData; //holds JSON info
+    private string gameDataProjectFilePath = "/StreamingAssets/data/json"; //json file path
+
+    [MenuItem("Window/Game Data Editor")] //create a menu item attribute(GUI)
+    static void Init()
+    {
+        EditorWindow.GetWindow(typeof(GameDataEditor)).Show(); //
+    }
+
+    void OnGUI()//similar to update
+    {
+        if (gameData != null) //is data loaded?
+        {
+            SerializedObject serializedObject = new SerializedObject(this); //this = GmaeDataEditor
+            SerializedProperty serializedProperty = serializedObject.FindProperty("gameData"); // search through serialized object "gameData"
+            EditorGUILayout.PropertyField(serializedProperty, true); //pass in serialized proerty to display children 
+
+            serializedObject.ApplyModifiedProperties(); //incase user has changed anything update object
+
+            if (GUILayout.Button("Save data"))
+            {
+                SaveGameData();
+            }
+        }
+
+        if (GUILayout.Button("Load data"))
+        {
+            LoadGameData();
+        }
+    }
+
+    private void LoadGameData()
+    {
+        string filePath = Application.dataPath + gameDataProjectFilePath; //
+
+        if (File.Exists(filePath))
+        {
+            string dataAsJson = File.ReadAllText(filePath); //read/convert as JSON 
+            gameData = JsonUtility.FromJson<GameData>(dataAsJson); //Place into gamedata object
+        }
+        else
+        {
+            gameData = new GameData();
+        }
+    }
+
+    private void SaveGameData()
+    {
+
+        string dataAsJson = JsonUtility.ToJson(gameData);//serializee form JSON store as string
+
+        string filePath = Application.dataPath + gameDataProjectFilePath;
+        File.WriteAllText(filePath, dataAsJson);//loads or overites
+
+    }
+}
--- Assets/Scripts/Persistant/DataController.cs
+++ Assets/Scripts/Persistant/DataController.cs
@@ -1,22 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine; //to use unity API
+using UnityEngine.SceneManagement; //for loading scene
+using System.IO;// for files
 
 //manages all behavior
 public class DataController : MonoBehaviour
-{
-    public InstructionData[] allInstructionData; // size of the Instruction set
+{   //was public but now private
+    private InstructionData[] allInstructionData; // size of the Instruction set // set to private because of json
+    private string gameDataFileName = "data.json";
     // Start is called before the first frame update
     void Start()
     {
         DontDestroyOnLoad(gameObject);
+        LoadGameData();//Loads Json
         SceneManager.LoadScene ("HUD");
     }
 
     public InstructionData GetCurrentInstructionData()
     {
         return allInstructionData[0];// eventually we can use this to determine other data types from the round
+    }
+    private void LoadGameData()
+    {
+        string filePath = Path.Combine(Application.streamingAssetsPath, gameDataFileName); //where the JSON is
+        if (File.Exists(filePath))
+            {
+            string dataAsJson = File.ReadAllText(filePath); //read file store it as a string
+            GameData loadedData = JsonUtility.FromJson<GameData>(dataAsJson);//take string, deserialize it into a GameData object
+
+            allInstructionData = loadedData.allInstructionData; //make it available to use in the game (see datacontroller and gamedata; same variable)
+        }
+        else
+        {
+            Debug.LogError("Cannot load Game data");
+        }
     }
     // Update is called once per frame
     void Update()
